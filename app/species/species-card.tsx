fb299f80c0764bc5ae2c0852155091f40bdc66b0
Cathy Hou <cathyhou@college.harvard.edu>
2023-09-20 14:10:15 -0400

    bug fixes

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -181,7 +181,7 @@ export default function SpeciesCard(species: Species) {
       })
     }
 
-    const {data, error} = await supabase
+    const {error} = await supabase
       .from('species')
       .update({
         author: currentId.user.id,
